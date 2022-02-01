6e1a70e8c1ac006ef8a6c4aa4c59a1d4abbadabd
Anamika Hazari <ahazari@forbestech.com>
2022-02-01 18:35:27 +0530

    updated classname attribute

--- src/component/registerinpage/Registerpage.jsx
+++ src/component/registerinpage/Registerpage.jsx
@@ -3,10 +3,10 @@ import './registerinpage.css'
 export default function Registerpage() {
   return <div className='flex flex-col justify-center items-center'>
       <div>      <input type="text" id="userName"className='bg-transparent text-white cursor-pointer belowBorderLine p-3 text-gray-600' placeholder='Enter User Name'></input>
-      <label for="userName"><i class="fas fa-user colorWhite cursor-pointer"></i></label></div>
+      <label for="userName"><i className="fas fa-user colorWhite cursor-pointer"></i></label></div>
       <div>  <input type="text" id="Password"className='bg-transparent text-white cursor-pointer belowBorderLine p-3 text-gray-600' placeholder='Enter Password'></input>
-      <label for="Password"><i class="fas fa-lock fill-red-300 colorWhite cursor-pointer"></i></label></div>
+      <label for="Password"><i className="fas fa-lock fill-red-300 colorWhite cursor-pointer"></i></label></div>
       <div>      <input type="text" id="rePassword"className='bg-transparent text-white cursor-pointer belowBorderLine p-3 text-gray-600' placeholder='Re Enter Password'></input>
-      <label for="rePassword"><i class="fas fa-key colorWhite cursor-pointer"></i></label></div>
+      <label for="rePassword"><i className="fas fa-key colorWhite cursor-pointer"></i></label></div>
   </div>;
 }
